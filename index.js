3090bb6d3d2aa62edd72887b5060b834b825eadf
murtaza652 <43542117+murtaza652@users.noreply.github.com>
2023-06-27 00:34:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,61 +6,95 @@ const itemList1 = document.getElementById('item');
 
 myForm.addEventListener('submit', onSubmit);
 itemList.addEventListener('click', removeItem);
-  
-function onSubmit(e){
-    e.preventDefault();
-    axios.post('https://crudcrud.com/api/618b6c3f7742432c8bfed904a93866f7/todos',{
-      name:TodoName.value,
-      desc:TodoDesc.value,
-    })
-      .then(re=> showOnscreen(re.data,"todos remaining"))
-      .catch(e=> console.log(e))
-    
-  }  
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-      var li = e.target.parentElement;
-      let link="https://crudcrud.com/api/618b6c3f7742432c8bfed904a93866f7/todos/"+e.target.id;
-      
-      axios.delete(link);
-      itemList.removeChild(li); 
-  }
-  if(e.target.classList.contains('edit')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-      li.removeChild(li.firstChild.nextSibling.nextSibling.nextSibling.nextSibling);
-      li.removeChild(li.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling);
-      itemList1.appendChild(li);
-      let link="https://crudcrud.com/api/618b6c3f7742432c8bfed904a93866f7/todos/"+ e.target.id;
-      axios.delete(link);
+
+async function onSubmit(e) {
+  e.preventDefault();
+  try {
+    const response = await axios.post('https://crudcrud.com/api/5e20656a6bae47fd8aef27e159d1855a/todos', {
+      name: TodoName.value,
+      desc: TodoDesc.value,
+    });
+    showOnscreen(response.data, "todos remaining");
+  } catch (error) {
+    console.log(error);
   }
 }
-function showOnscreen(user,x)
-{
-  // Create new li element
+
+async function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    var li = e.target.parentElement;
+    let link = "https://crudcrud.com/api/5e20656a6bae47fd8aef27e159d1855a/todos/" + e.target.id;
+
+    try {
+      await axios.delete(link);
+      itemList.removeChild(li);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  if (e.target.classList.contains('edit')) {
+    var li = e.target.parentElement;
+    itemList.removeChild(li);
+    let link = "https://crudcrud.com/api/5e20656a6bae47fd8aef27e159d1855a/todos/" + e.target.id;
+    let obs={};
+    await axios.get(link)
+      .then( res=> {obs.name=res.data.name;
+                    obs.desc=res.data.desc})
+      .catch(e=> console.log(e))
+    axios.post('https://crudcrud.com/api/5e20656a6bae47fd8aef27e159d1855a/todosdone', {
+      name: obs.name,
+      desc: obs.desc
+    })
+      .then(re=> showOnscreen(re.data, "todos completed"))
+      .catch(e=> console.log(e))
+    try {
+      await axios.delete(link);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+window.addEventListener('DOMContentLoaded',()=>{
+  axios.get('https://crudcrud.com/api/5e20656a6bae47fd8aef27e159d1855a/todos')
+    .then( res=> {
+      for(var i=0; i<res.data.length; i++) { 
+        showOnscreen(res.data[i],"todos remaining");
+    }})
+  axios.get('https://crudcrud.com/api/5e20656a6bae47fd8aef27e159d1855a/todosdone')
+    .then( res=> {
+      for(var i=0; i<res.data.length; i++) { 
+        showOnscreen(res.data[i],"todos completed");
+    }})
+
+})
+function showOnscreen(user, x) {
   var li = document.createElement('li');
-  // Add class
   li.className = x;
-  // Add text node with input value
   li.appendChild(document.createTextNode(user.name));
   li.appendChild(document.createTextNode("-"));
   li.appendChild(document.createTextNode(user.desc));
   li.appendChild(document.createTextNode(" "));
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-  var completedBtn= document.createElement('button');
-  // Add classes to del button
-  deleteBtn.className = 'btn-sm float-right delete';
-  deleteBtn.id=user._id;
-  completedBtn.className="btn-sm float-right edit"
-  completedBtn.id=user._id;
-  // Append text node
-  completedBtn.appendChild(document.createTextNode('Done'));  
-  deleteBtn.appendChild(document.createTextNode('remove'));
-  // Append button to li
-  li.appendChild(completedBtn);
-  li.appendChild(document.createTextNode(" "));
-  li.appendChild(deleteBtn);
-  // Append li to list
-  itemList.appendChild(li);
+
+  if(x=="todos remaining")
+  { 
+    var deleteBtn = document.createElement('button');
+    var completedBtn = document.createElement('button');
+    deleteBtn.className = 'btn-sm float-right delete';
+    deleteBtn.id = user._id;
+    completedBtn.className = "btn-sm float-right edit";
+    completedBtn.id = user._id;
+    completedBtn.appendChild(document.createTextNode('Done'));
+    deleteBtn.appendChild(document.createTextNode('remove'));
+
+    li.appendChild(completedBtn);
+    li.appendChild(document.createTextNode(" "));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+  }
+  if(x=="todos completed")
+  {
+    li.id=user._id;
+    itemList1.appendChild(li);
+  }
+  
 }
